07cc58a9f90e78c3b1095325fc6a70dcb9d05784
josehomero <jose.homero.rios@outlook.com>
2021-01-09 20:25:52 -0800

    updated

--- Program.cs
+++ Program.cs
@@ -6,31 +6,28 @@ namespace C_sharp_battleship
     {
         static void Main(string[] args)
         {
-            string appName = "C Sharp Battleship";
-            string appAuthor = "Homero Rios";
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("{0} by {1}", appName, appAuthor);
-
-            Console.ResetColor();
-
-            Console.WriteLine("Would you like a tutorial? y or n");
-            string input = Console.ReadLine();
-
-            while((input != "y") || (input != "n")){
-                if ((input != "y") || (input != "n"))
+            {
+                string appName = "C Sharp Battleship";
+                string appAuthor = "Homero Rios";
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("{0} by {1}", appName, appAuthor);
+                Console.ResetColor();
+                Console.WriteLine("Would you like a tutorial? y or n");
+                string input = Console.ReadLine();
+                while ((input != "y") || (input != "n"))
                 {
-                    Console.WriteLine("Please type in either y or n");
+                    Console.WriteLine("please type in y or n");
+                    input = Console.ReadLine();
                 }
-            }
 
-                 if (input == "y")
-            {
-                Console.WriteLine("you have chosen turorial");
-            }
-            else if (input == "n")
-            {
-                Console.WriteLine("you have chosen not to view the tutorial");
+                if (input == "y")
+                {
+                    Console.WriteLine("you have chosen tutorial");
+                }
+                else if (input == "n")
+                {
+                    Console.WriteLine("you have chosen not to view the tutorial");
+                }
             }
 
 
