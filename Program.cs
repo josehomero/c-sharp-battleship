33ce45ad247f8794b37c9c86fbb02ec8ddc73bc4
josehomero <jose.homero.rios@outlook.com>
2021-01-10 20:51:25 -0800

    updated

--- Program.cs
+++ Program.cs
@@ -22,7 +22,27 @@ namespace C_sharp_battleship
 
                 if (input == "y")
                 {
-                    Console.WriteLine("you have chosen tutorial");
+                    Console.WriteLine("directions: The Game is simple, you will be prompted to select a point on a 10 * 10 grid.The point " +
+                        "is made up of an X - value and a Y - value that reprsesents square of the grid... Please see bellow illustration\n");
+
+                    Console.WriteLine("10 - - - - - - - - - -");
+                    Console.WriteLine("9  - - - - - - - - - -");
+                    Console.WriteLine("8  - - - - - - - - - -");
+                    Console.WriteLine("7  - - - - - - - - - -");
+                    Console.WriteLine("6  - - - - - - - - - -");
+                    Console.WriteLine("5  - - - - - - - - - -");
+                    Console.WriteLine("4  - - - - - - - - - -");
+                    Console.WriteLine("3  - - - - - - - - - -");
+                    Console.WriteLine("2  - - - - - - - - - -");
+                    Console.WriteLine("1  - - - - - - - - - -");
+                    Console.WriteLine("0  1 2 3 4 5 6 7 8 9 10\n");
+
+                    Console.WriteLine("Once the point is selected you will be promped with a hit or a miss.You will be given a total of 3" +
+                        " guesses. if you do not destroy the enemy vessel... You are probably a fit for the 1588 spanish armado and lose " +
+                        "the game. However if you annihilate the enenmy which takes up 5 grid spaces you win After which you can gloat " +
+                        "to your friends and family, and garner the respect of your peers " +
+                        "press any key and let's fire up the boilers and ship off!");
+
                 }
                 else if (input == "n")
                 {
